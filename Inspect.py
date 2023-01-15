5909a7e19364c1d3a0007e8eda64b57294b05c90
ToddJBrewer <31729740+ToddJBrewer@users.noreply.github.com>
2023-01-15 11:22:03 -0800

    Initial Commit

---
+++ Inspect.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import git
+import time
+
+def main():
+	# Check for proper input
+	if (len(sys.argv) != 2):
+		print("Error: Program is intended to take one file path as an argument")
+		return
+	git_dir = sys.argv[1]
+
+	# Check if the path exists and is a git repo
+	if os.path.exists(git_dir):
+  		try:
+  			git_inspect(git_dir)
+  		except:
+  			print("Error: Directory is not a git repository")
+	else:
+  		print("Error: The path '{}' does not exist.".format(git_dir))
+
+def git_inspect(git_dir):
+	# Create repo object
+	repo = git.Repo(git_dir)
+	# Print active branch
+	print("active branch:", repo.active_branch)
+	# Check whether repository files have been modified
+	print("local changes:", repo.is_dirty(untracked_files=True))
+	# Check whether the current head commit was authored in the last week
+	time_change = True if (round(time.time()) - repo.head.commit.committed_date < 60 * 60 * 24 * 7) else False
+	print("recent commit: ", time_change)
+	# Check whether current head commit was authored by Rufus
+	rufus = True if repo.head.commit.author.name == "Rufus" else False
+	print("blame Rufus:", rufus)
+
+main()
---
+++ README.md
@@ -0,0 +1,22 @@
+# Github Inspector
+
+Github Inspector is a Python program that takes the path to a local github repository as a command line argument, and prints facts about the repo.
+
+## Installation
+
+Github Inspector relies on the GitPython library [pip](https://pip.pypa.io/en/stable/) to install GitPython.
+
+```bash
+python -m pip install gitpython
+```
+
+## Usage
+
+```python
+# prints various information about the repo
+python3 Inspect.py C:/Users/user-name/Documents/repo-name
+```
+
+## Author
+
+This program was written by Todd Brewer
